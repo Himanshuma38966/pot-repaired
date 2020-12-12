b7b817a12fec355eeac0c8be1a18033b18bc462e
Himanshuma38966 <74598469+Himanshuma38966@users.noreply.github.com>
2020-12-12 19:07:45 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,30 @@
+class Ball{
+    
+    constructor(x,y,radius,color) {
+        var options = { isStatic:false, 
+            restitution:1, 
+            
+             density:1.2 }
+
+        this.body = Bodies.circle(x,y,radius,options);
+        this.radius= radius;
+        this.color = color;
+       
+        World.add(world,this.body);
+    }
+
+    display() {
+        var pos=this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        rotate(this.body.angle);
+        ellipseMode(RADIUS);
+        
+        
+        ellipse(0,0,this.radius,this.radius);
+        
+        pop()
+        
+        
+    }
+}
